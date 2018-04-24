7ab5a9c4757bcf9a21427bd5a0dcbe8c7a869044
Josh Stoutenburg <jehoshua02@gmail.com>
2018-04-24 15:31:34 -0600

    Custom HTML Example

--- examples/html.php
+++ examples/html.php
@@ -1,13 +1,13 @@
 <?php
 
-require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 use ChromeDevtoolsProtocol\Context;
 use ChromeDevtoolsProtocol\Instance\Launcher;
 use ChromeDevtoolsProtocol\Model\Page\PrintToPDFRequest;
-use ChromeDevtoolsProtocol\Model\Page\NavigateRequest;
+use ChromeDevtoolsProtocol\Model\Page\SetDocumentContentRequest;
 
-unlink(__DIR__ . '/test.pdf');
+@unlink(__DIR__ . '/../test.pdf');
 
 // context creates deadline for operations
 $ctx = Context::withTimeout(Context::background(), 30 /* seconds */);
@@ -23,13 +23,17 @@ try {
 
 	$devtools = $tab->devtools();
 	try {
-		$devtools->page()->enable($ctx);
-		$devtools->page()->navigate($ctx, NavigateRequest::builder()->setUrl("https://www.google.com/")->build());
+        $devtools->page()->enable($ctx);
+        $frameId = $devtools->page()->getFrameTree($ctx)->frameTree->frame->id;
+        $devtools->page()->setDocumentContent($ctx, SetDocumentContentRequest::fromJson((object) [
+            'frameId' => $frameId,
+            'html' => '<!DOCTYPE html><html><head></head><body><h1>Hello World</h1></body></html>'
+        ]));
 		$devtools->page()->awaitLoadEventFired($ctx);
         $data = $devtools->page()->printToPDF($ctx, PrintToPDFRequest::fromJson((object) [
-            'displayHeaderFooter' => true
+            'displayHeaderFooter' => false
         ]))->data;
-        file_put_contents(__DIR__ . '/test.pdf', base64_decode($data));
+        file_put_contents(__DIR__ . '/../test.pdf', base64_decode($data));
 	} finally {
 		// devtools client needs to be closed
 		$devtools->close();
